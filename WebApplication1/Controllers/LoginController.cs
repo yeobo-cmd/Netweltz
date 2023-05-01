2f854f3e12f1c467839443b57d22a947e2585d75
Administrator <Administrator@DESKTOP-8DR1E3T>
2023-05-01 13:59:23 +0800

    Add project files.

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public List<Usermodel> PutValue()
+        {
+            var users = new List<Usermodel>
+            {
+                new Usermodel { Id = 1, username = "admin", password = "admin123" } 
+            };
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(Usermodel user)
+        {
+            var u = PutValue();
+            var un= u.Where(u=>u.username.Equals(user.username));
+            var userpassword = u.Where(u => u.password.Equals(user.password));
+            if (userpassword.Count() == 1)
+            {
+                ViewBag.message = "Login Success";
+                return View("LoginSuccess");
+            } 
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+     }
+}
---
+++ WebApplication1/Models/Usermodel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Usermodel
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
